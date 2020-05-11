3a46f722ddbf0b9262dfecc8717e57bd09a95048
fattym <jnganga288@gmail.com>
2020-05-11 14:31:52 +0300

    added request for the api

--- flask3/routes.py
+++ flask3/routes.py
@@ -7,12 +7,12 @@ from flask3.forms import RegistrationForm, LoginForm,UpdateAccountForm,PostForm
 from flask3.models import User,Post
 from flask_login import login_user,current_user,logout_user,login_required
 
-
 @app.route('/')
 @app.route('/home')
 def home():
     page = request.args.get('page', 1, type=int)
     posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=5)
+   
     return render_template('home.html',posts= posts)
 
 @app.route('/about')
